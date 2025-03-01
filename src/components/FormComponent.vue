12adc386fc57a46ba8787fdd0b8d82e9e72336ad
alexalaska90 <chrisalexonn@gmail.com>
2025-03-01 12:08:37 +0300

    components added

---
+++ src/components/FormComponent.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import AccountComponent from './AccountComponent.vue'
+</script>
+
+<template>
+  <section>
+    <div class="row justify-start">
+      <h5 class="text-weight-bold">Учётные записи</h5>
+    </div>
+    <AccountComponent />
+  </section>
+</template>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import FormComponent from '../components/FormComponent.vue'
+</script>
+
 <template>
-  <q-page class="row items-center justify-evenly"></q-page>
+  <q-page class="row items-center justify-evenly">
+    <FormComponent />
+  </q-page>
 </template>
