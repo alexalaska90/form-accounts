7beacdce6a674b9224a85cc599614f5d4137f1e2
alexalaska90 <chrisalexonn@gmail.com>
2025-03-02 14:31:50 +0300

    added inputs, formatted

--- src/components/AccountsComponent.vue
+++ src/components/AccountsComponent.vue
@@ -1,16 +1,40 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+
+const text = ref<string>('')
+
+const model = ref<string>('Локальная')
+const options = reactive<string[]>(['Локальная', 'LDAP'])
+const isPwd = ref<boolean>(true)
+</script>
+
 <template>
-  <div class="row q-mt-md">
+  <div class="row q-gutter-x-md items-center no-wrap q-mt-md">
     <div class="col-3 text-subtitle2 text-weight-bold text-grey-7">Метки</div>
     <div class="col-2 text-subtitle2 text-weight-bold text-grey-7">Тип записи</div>
     <div class="col-3 text-subtitle2 text-weight-bold text-grey-7">Логин</div>
     <div class="col-3 text-subtitle2 text-weight-bold text-grey-7">Пароль</div>
     <div class="col-1"></div>
   </div>
-  <div class="row q-mt-md">
-    <div class="col-3"></div>
-    <div class="col-2"></div>
-    <div class="col-3"></div>
-    <div class="col-3"></div>
-    <div class="col-1"></div>
+  <div class="row q-gutter-x-md items-center no-wrap q-mt-md">
+    <div class="col-3">
+      <q-input placeholder="Значение" outlined v-model="text" dense />
+    </div>
+    <div class="col-2">
+      <q-select placeholder="Значение" outlined v-model="model" :options="options" dense />
+    </div>
+    <div class="col-3">
+      <q-input placeholder="Значение" outlined v-model="text" dense />
+    </div>
+    <div class="col-3">
+      <q-input placeholder="Значение" outlined v-model="text" dense :type="isPwd ? 'password' : 'text'">
+        <template v-slot:append>
+          <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd" />
+        </template>
+      </q-input>
+    </div>
+    <div class="col-1">
+      <q-btn flat color="negative" class="q-pa-xs" icon="delete" />
+    </div>
   </div>
 </template>
--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -6,16 +6,13 @@ import AccountsComponent from './AccountsComponent.vue'
   <section class="main_section">
     <div class="row items-center q-gutter-x-md">
       <h5 class="text-weight-bold q-my-none">Учётные записи</h5>
-        <q-btn
-          outline
-          padding="md"
-          color="primary"
-          icon="add"
-      />
+      <q-btn outline padding="md" color="primary" icon="add" />
     </div>
-    <q-card flat class="row items-center q-gutter-x-sm bg-blue-grey-1 q-mt-lg no-wrap">
-      <q-icon name="help" color="dark" size="md" />
-      <span>Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;</span>
+    <q-card flat class="bg-blue-grey-1 q-mt-lg">
+      <div class="row items-center q-gutter-x-sm no-wrap">
+        <q-icon name="help" color="dark" size="md" />
+        <span>Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;</span>
+      </div>
     </q-card>
     <AccountsComponent />
   </section>
@@ -24,7 +21,7 @@ import AccountsComponent from './AccountsComponent.vue'
 <style lang="scss">
 .main_section {
   width: 100%;
-  max-width: 768px;
+  max-width: 1368px;
   padding: 0 16px;
 }
 </style>
